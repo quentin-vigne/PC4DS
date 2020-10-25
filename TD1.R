1696dce69999d61bb354ec08fdec634cbcd15d38
quentin-vigne <59710953+quentin-vigne@users.noreply.github.com>
2020-10-26 00:40:57 +0100

    update

--- TD1.R
+++ TD1.R
@@ -27,16 +27,39 @@ optimise(f,c(-6,4))
 # 5)
 ## Exercice 3 ####
 # 1)
-simuData <- function(n) rnorm(n)
+simuData <- function(n){
+  set.seed(2020)
+  runif(n)
+} 
 # 2)
-perte <- function(s, y, p) (sum(abs(s-y) ^ p)) ^ (1 / p)
+perte <- function(s, y=simuData(20), p=2) (sum(abs(s-y) ^ p)) ^ (1 / p)
+perte1 <- function(s, y=simuData(20), p=1) (sum(abs(s-y) ^ p)) ^ (1 / p)
 
 # 3)
 y <- simuData(20)
-for (p in c(1, 2, 5, 1/2)){
-  print(paste("Computation for p = ", p))
-  print(optimise(f = perte, interval = range(y), y=y, p=p))
+
+ps <- c(1, 2, 5, 1/2)
+lps <- length(ps)
+results <- data.frame(p         = numeric(lps),
+                      minimum   = numeric(lps),
+                      objective = numeric(lps))
+
+for (p in seq_along(ps)) {
+  print(paste("Begin computation for p = ", ps[p]))
+  res <- optimise(f = perte, interval = range(y), y = y, p = ps[p])
+  results[p, ] <- c(ps[p], res)
 }
+# 4)
+vectorized_perte <- Vectorize(perte)
+curve(vectorized_perte, from = 0, to = 2)
+vectorized_perte1 <- Vectorize(perte1)
+curve(vectorized_perte1, from = 0, to = 2)
+abline(h=results$objective[2], col='red')
 
+# 5)
+median(y) # should be minimum for p = 1
+mean(y)   # should be minimum for p = 2
 
+# 6)
+abline(h=results$objective[1], col='blue')
 ## Exercice 4 ####
